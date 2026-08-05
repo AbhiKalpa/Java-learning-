775a033f9b90892b30cb652db9236516c4705b18
Abhi.G <ghuteabhi9@gmail.com>
2026-08-05 17:46:20 +0530

    docs: add while loop notes and sum program

--- 01-loops/while_loop/Sum_of_1_to_50.java
+++ 01-loops/while_loop/Sum_of_1_to_50.java
@@ -1,12 +1,15 @@
 package while_loop;
+import java.util.Scanner;
 
 public class Sum_of_1_to_50 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
      int i=0;
        while(i<=50){
            for(i =0; i<=50 ;i++) {
                System.out.println(i);
            }
        }
+       sc.close();
     }
 }
