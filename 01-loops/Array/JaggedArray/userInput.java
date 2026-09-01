d1abf655f6ad02519ba3aa63b46a667f9c0b9ffd
Abhi.G <ghuteabhi9@gmail.com>
2026-09-01 23:59:48 +0530

    learned arrays well and also seen how we can take user input and save inside the arrays you know java is starting to feel fun now

--- 01-loops/Array/JaggedArray/userInput.java
+++ 01-loops/Array/JaggedArray/userInput.java
@@ -1,4 +1,28 @@
 package Array.JaggedArray;
 
+import java.util.Scanner;
+
 public class userInput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int row;
+        int colm;
+
+        System.out.println("Enter the no of rows : ");
+        row = sc.nextInt();
+
+        System.out.println("Enter the no of columns : ");
+        colm = sc.nextInt();
+
+        int arr [][] = new int[row][colm];
+
+        for(int i = 0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println(" ");
+        }
+
+    }
 }
