a6c5a783274c5fbde1aa7751d84300c1fd2a015b
Abhi.G <ghuteabhi9@gmail.com>
2026-09-02 22:34:18 +0530

    started with Strings well String looks easy but in back-groud buddy its little bit complex but we will move one step closer to learn it not in rush way but in better way seeya

---
+++ Strings/src/AboutString.java
@@ -0,0 +1,13 @@
+public class AboutString {
+    public static void main(String[] args){
+        String name = "abhi";
+        name = name+" don";
+
+        System.out.println("hello "+name);
+
+        String str1 = "Navin";
+        String str2 = "Navin";
+
+        System.out.println(str1 == str2);
+    }
+}
---
+++ Strings/src/intro.java
@@ -0,0 +1,18 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+public class intro {
+    public static void main(String[] args){
+        System.out.println("Lets start with the Strings NOw");
+
+
+        //we can declare strings with simple method and with class method
+//        1) simple way
+        String str = "Abhi";
+        System.out.println(str+" Welcome");
+
+//        2) Class way
+        String str1 = new String("Rohan");
+        System.out.println(str1);
+    }
+}
